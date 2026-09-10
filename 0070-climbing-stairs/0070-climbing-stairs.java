9c1028167c8ad48a1ee41ff714c60ef4a8805d62
Naman Sahu <namansahu5542@gmail.com>
2026-09-10 16:02:39 +0530

    Time: 0 ms (100%), Space: 42.2 MB (17.29%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 2) {
+            return 2;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        int a = 1, b = 2, sum = 0;
+        for (int i = 3; i <= n; i++) {
+            sum = a + b;
+            a = b;
+            b = sum;
+        }
+        return sum;
+    }
+}
